445cf7aa0a9f817f7d371c97521f4f0cccead442
Evgeny Karkan <evgenykarkan@gmail.com>
2015-11-12 01:07:33 +0200

    Initial commit.

---
+++ IAPSandBoxApp/IAPSandBoxApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAPSandBoxApp
+//
+//  Created by Evgeny Karkan on 11/12/15.
+//  Copyright © 2015 Evgeny Karkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
